a9480321344625a7cf1f0a16ffb37ddf10973f91
Дмитрий Криволапов <dimakrivolapov@gmail.com>
2020-10-25 12:17:47 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while(1)
+  { let flag = false;
+    bracketsConfig.forEach(element => {
+      if(str.includes(element.join('')))
+      {
+        flag = true;
+        str = str.replace(element.join(''),'');
+      }
+    });
+    if(flag==false)
+      break;
+  }
+  if(str.length==0)
+    return true;
+  return false;
 }
